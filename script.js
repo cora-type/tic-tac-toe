db3c360f75cec808ecec3df95bb21e9ff667401b
shy realm <gtsbyxx@gmail.com>
2022-04-28 20:03:18 -0400

    added marker function

--- script.js
+++ script.js
@@ -1,65 +1,99 @@
+let player1;
+let player2;
+let currentPlayer;
+
 const gameBoard = (() => {
-  let gameboard = ["X", "X", "O", "X", "X", "O", "O", "O", "O"];
-  // let gameboardFinal = Array[9];
+  let gameboard = Array(9).fill(undefined);
+  let boxes = document.querySelectorAll(".box");
+
+  const marker = () => {
+    boxes.forEach((result) => {
+      result.addEventListener("click", function _switcher() {
+        switch (currentPlayer.mark) {
+          case "X":
+            gameboard.splice(result.id, 1, "X");
+            displayController.displayUpdate();
+            currentPlayer = player2;
+            this.removeEventListener("click", _switcher);
+            break;
+          case "O":
+            gameboard.splice(result.id, 1, "O");
+            displayController.displayUpdate();
+            currentPlayer = player1;
+            this.removeEventListener("click", _switcher);
+            break;
+        }
+      });
+    });
+  };
   return {
     gameboard,
+    marker,
   };
 })();
 
-// const player = (name, symbol) => {
-//   const sayHello = () => console.log("hello!");
-//   return { name, age, sayHello };
-// };
-
 const displayController = (() => {
-  let currentGame = gameBoard.gameboard; // grab the current gameboard array
-  currentGame.forEach((result, idx) => {
-    // for each element in the gameboard
-    if (!(result == "")) {
-      //if the element isnt empty
-      let box = document.getElementById(idx); //get the div linked to the current element ID
-      let image = document.createElement("img"); //create the tag
-
-      if (result == "X") {
-        // if it's an X, put image ex
-        image.style.width = "100px";
-        image.src = "images/ex.svg";
-        box.appendChild(image);
-      } else {
-        // if it's a circle, put a circle
-        image.style.width = "50px";
-        image.src = "images/circle.svg";
-        box.appendChild(image);
+  let gameboard = gameBoard.gameboard; // grab the current gameboard array
+  const displayUpdate = () => {
+    gameboard.forEach((result, idx) => {
+      let box = document.getElementById(idx);
+      // for each element in the gameboard, if the array index isn't empty and it doesnt already have a mark, add a mark
+      if (!result == "" && !box.hasChildNodes()) {
+        let image = document.createElement("img");
+        if (result == "X") {
+          image.style.width = "100px";
+          image.src = "images/ex.svg";
+          box.appendChild(image);
+        } else if (result == "O") {
+          image.style.width = "50px";
+          image.src = "images/circle.svg";
+          image.style.cursor = "default";
+          box.appendChild(image);
+        }
       }
-    }
-  });
-  return {};
+    });
+  };
+
+  return { displayUpdate };
 })();
 
-// const displayController = ((player, gameboard) => {
-//   let activeSymbol = player.symbol // the current symbol to input
-//   let boxes = document.querySelectorAll(".box"); // select all boxes
-//   boxes.addEventListener('click', function (){ // add an event listener to all boxes
-//     const symbol = document.createElement('svg')
-//   })
-//   // boxes.forEach((result, idx) => {
+//player factory
+const player = (name, mark) => {
+  return {
+    name,
+    mark,
+  };
+};
 
-//   // });
-// })();
+//starts the game based on inputs
 
-// // const jeff = personFactory("jeff", 27);
+const gameInitializer = (() => {
+  const userName = document.getElementById("user1");
+  const user1Submit = document.getElementById("user1Button");
 
-// const player = (name, symbol) => {
-//   const sayHello = () => console.log('hello!');
-//   return { name, symbol, sayHello };
-// };
+  const opponentName = document.getElementById("opponent");
+  const opponentSubmit = document.getElementById("opponentButton");
 
-// const jeff = player('jeff', 27);
+  const userMarkX = document.getElementById("toggle-on");
 
-// console.log(jeff.name); // 'jeff'
+  //create player
+  user1Submit.addEventListener("click", function () {
+    let name = userName.value;
+    if (userMarkX.checked) {
+      player1 = player(name, "X");
+    } else player1 = player(name, "O");
+  });
 
-// jeff.sayHello(); // calls the function and logs 'hello!'
+  //create opponent
+  opponentSubmit.addEventListener("click", function () {
+    let name = opponentName.value;
+    if ((player1.mark = "X")) {
+      player2 = player(name, "O");
+    } else player2 = player(name, "X");
 
-// // window.addEventListener("load", (event) => {
-// //   displayController();
-// // });
+    currentPlayer = player1;
+    gameBoard.marker();
+  });
+
+  return {};
+})();
