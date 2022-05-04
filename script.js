2cacec97af4d60f8dafcb038bb4317ea487bae3a
shy realm <gtsbyxx@gmail.com>
2022-05-04 09:46:53 -0400

    got minimax to work, IIFE's cause errors

--- script.js
+++ script.js
@@ -16,11 +16,16 @@ const winners = [
 ];
 
 const gameBoard = (() => {
-  let gameboard = Array(9).fill(undefined);
+  let gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   let boxes = document.querySelectorAll(".box");
   const versus = document.querySelector(".versus");
 
+  function onlyString(array) {
+    return array.every((element) => {
+      return typeof element === "string";
+    });
+  }
   // determine winner
   const winner = (func) => {
     for (let i = 0; i < winners.length; i++) {
@@ -28,10 +33,9 @@ const gameBoard = (() => {
       let first = gameBoard.gameboard[r[0]];
       let second = gameBoard.gameboard[r[1]];
       let third = gameBoard.gameboard[r[2]];
-      let j = gameBoard.gameboard;
 
       // if all 3 arent undefined, compare them, if theyre all the same mark, winner based on mark
-      if (first && second && third) {
+      if (isNaN(first) && isNaN(second) && isNaN(third)) {
         if (first === second && second === third) {
           boxes.forEach((box) => {
             box.removeEventListener("click", func);
@@ -40,7 +44,7 @@ const gameBoard = (() => {
           toggle //declare winner depending on toggle position
             ? (versus.innerText = player2.name + ", wins")
             : (versus.innerText = player1.name + ", wins");
-        } else if (winnerFound == false && j.includes(undefined) == false) {
+        } else if (winnerFound == false && onlyString(gameBoard.gameboard)) {
           //if there are no empty spaces left, and a winner still hasnt been found
           winnerFound = false;
           versus.innerText = "tie";
@@ -92,19 +96,13 @@ const gameBoard = (() => {
         currentPlayer = playerSwapper();
         event.target.removeEventListener("click", aiMarker);
         break;
-      case player2: // if the currentPlayer is the computer, use minimax to mark?
-        // splice the return value from running minimax with the current board & computer as the player
-        let bestSpot = computer.minimax(gameBoard.gameboard, computer.aiPlayer);
-        console.log(bestSpot.index);
-        // gameBoard.gameboard.splice(
-        //   event.target.id,
-        //   1,
-        //   computer.minimax(gameBoard.gameboard, player2.mark)
-        // );
-        // gameBoard.winner(aiMarker);
-        // displayController.displayUpdate();
-        // currentPlayer = playerSwapper();
-        // event.target.removeEventListener("click", aiMarker);
+      case player2:
+        let r = minimax(gameBoard.gameboard, player2.mark);
+        gameBoard.gameboard.splice(r.index, 1, player2.mark);
+        gameBoard.winner(aiMarker);
+        displayController.displayUpdate();
+        currentPlayer = playerSwapper();
+        boxes[r.index].removeEventListener("click", aiMarker);
         break;
     }
   }
@@ -194,7 +192,7 @@ const gameInitializer = (() => {
     markers.forEach((marker) => {
       marker.remove();
     });
-    gameBoard.gameboard = Array(9).fill(undefined);
+    gameBoard.gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   };
 
   //reset game
@@ -207,7 +205,7 @@ const gameInitializer = (() => {
     markers.forEach((marker) => {
       marker.remove();
     });
-    gameBoard.gameboard = Array(9).fill(undefined);
+    gameBoard.gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     gameBoard.marker(gameBoard.playerMarker); // initializes gameboard for marking
     displayController.displayUpdate();
   };
@@ -242,8 +240,8 @@ const gameInitializer = (() => {
       : (player2 = player("computer", "O"));
 
     currentPlayer = player1;
-    computer.huPlayer = player1.mark;
-    computer.aiPlayer = player2.mark;
+    huPlayer = player1.mark;
+    aiPlayer = player2.mark;
 
     displayOpponentName.innerText = player2.name + ", " + player2.mark;
     gameBoard.marker(gameBoard.aiMarker);
@@ -255,103 +253,95 @@ const gameInitializer = (() => {
   return {};
 })();
 
-//AI PLAYER
+let huPlayer;
+let aiPlayer;
 
-const computer = (() => {
-  //human
-  let huPlayer;
-  //ai
-  let aiPlayer;
+// the main minimax function
+function minimax(newBoard, player) {
+  //available spots
+  var availSpots = emptyIndexies(newBoard);
 
-  // the main minimax function
-  function minimax(newBoard, player) {
-    //available spots
-    var availSpots = emptyIndexies(newBoard);
+  // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+  if (winning(newBoard, huPlayer)) {
+    return { score: -10 };
+  } else if (winning(newBoard, aiPlayer)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
 
-    // checks for the terminal states such as win, lose, and tie and returning a value accordingly
-    if (winning(newBoard, huPlayer)) {
-      return { score: -10 };
-    } else if (winning(newBoard, aiPlayer)) {
-      return { score: 10 };
-    } else if (availSpots.length === 0) {
-      return { score: 0 };
-    }
+  // an array to collect all the objects
+  var moves = [];
 
-    // an array to collect all the objects
-    var moves = [];
+  // loop through available spots
+  for (var i = 0; i < availSpots.length; i++) {
+    //create an object for each and store the index of that spot that was stored as a number in the object's index key
+    var move = {};
+    move.index = newBoard[availSpots[i]];
 
-    // loop through available spots
-    for (var i = 0; i < availSpots.length; i++) {
-      //create an object for each and store the index of that spot that was stored as a number in the object's index key
-      var move = {};
-      move.index = newBoard[availSpots[i]];
+    // set the empty spot to the current player
+    newBoard[availSpots[i]] = player;
 
-      // set the empty spot to the current player
-      newBoard[availSpots[i]] = player;
-
-      //if collect the score resulted from calling minimax on the opponent of the current player
-      if (player == aiPlayer) {
-        var result = minimax(newBoard, huPlayer);
-        move.score = result.score;
-      } else {
-        var result = minimax(newBoard, aiPlayer);
-        move.score = result.score;
-      }
-
-      //reset the spot to empty
-      newBoard[availSpots[i]] = move.index;
-
-      // push the object to the array
-      moves.push(move);
-    }
-
-    // if it is the computer's turn loop over the moves and choose the move with the highest score
-    var bestMove;
-    if (player === aiPlayer) {
-      var bestScore = -10000;
-      for (var i = 0; i < moves.length; i++) {
-        if (moves[i].score > bestScore) {
-          bestScore = moves[i].score;
-          bestMove = i;
-        }
-      }
+    //if collect the score resulted from calling minimax on the opponent of the current player
+    if (player == aiPlayer) {
+      var result = minimax(newBoard, huPlayer);
+      move.score = result.score;
     } else {
-      // else loop over the moves and choose the move with the lowest score
-      var bestScore = 10000;
-      for (var i = 0; i < moves.length; i++) {
-        if (moves[i].score < bestScore) {
-          bestScore = moves[i].score;
-          bestMove = i;
-        }
+      var result = minimax(newBoard, aiPlayer);
+      move.score = result.score;
+    }
+
+    //reset the spot to empty
+    newBoard[availSpots[i]] = move.index;
+
+    // push the object to the array
+    moves.push(move);
+  }
+
+  // if it is the computer's turn loop over the moves and choose the move with the highest score
+  var bestMove;
+  if (player === aiPlayer) {
+    var bestScore = -10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
       }
     }
-
-    // return the chosen move (object) from the array to the higher depth
-    return moves[bestMove];
-  }
-
-  // returns the available spots on the board
-  function emptyIndexies(board) {
-    return board.filter((s) => s == undefined);
-  }
-
-  // winning combinations using the board indexies for instace the first win could be 3 xes in a row
-  function winning(board, player) {
-    if (
-      (board[0] == player && board[1] == player && board[2] == player) ||
-      (board[3] == player && board[4] == player && board[5] == player) ||
-      (board[6] == player && board[7] == player && board[8] == player) ||
-      (board[0] == player && board[3] == player && board[6] == player) ||
-      (board[1] == player && board[4] == player && board[7] == player) ||
-      (board[2] == player && board[5] == player && board[8] == player) ||
-      (board[0] == player && board[4] == player && board[8] == player) ||
-      (board[2] == player && board[4] == player && board[6] == player)
-    ) {
-      return true;
-    } else {
-      return false;
+  } else {
+    // else loop over the moves and choose the move with the lowest score
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
     }
   }
 
-  return { minimax, huPlayer, aiPlayer, emptyIndexies, winning };
-})();
+  // return the chosen move (object) from the array to the higher depth
+  return moves[bestMove];
+}
+
+// returns the available spots on the board
+function emptyIndexies(board) {
+  return board.filter((s) => s != "O" && s != "X");
+}
+
+// winning combinations using the board indexies for instace the first win could be 3 xes in a row
+function winning(board, player) {
+  if (
+    (board[0] == player && board[1] == player && board[2] == player) ||
+    (board[3] == player && board[4] == player && board[5] == player) ||
+    (board[6] == player && board[7] == player && board[8] == player) ||
+    (board[0] == player && board[3] == player && board[6] == player) ||
+    (board[1] == player && board[4] == player && board[7] == player) ||
+    (board[2] == player && board[5] == player && board[8] == player) ||
+    (board[0] == player && board[4] == player && board[8] == player) ||
+    (board[2] == player && board[4] == player && board[6] == player)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
