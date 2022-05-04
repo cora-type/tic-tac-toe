2b63bebb5add345fc764a9a6e440c352a9f88518
shy realm <gtsbyxx@gmail.com>
2022-05-04 05:20:45 -0400

    added minimax computer functionality

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const gameBoard = (() => {
   const versus = document.querySelector(".versus");
 
   // determine winner
-  const winner = () => {
+  const winner = (func) => {
     for (let i = 0; i < winners.length; i++) {
       let r = winners[i]; //[1,2,3]
       let first = gameBoard.gameboard[r[0]];
@@ -34,7 +34,7 @@ const gameBoard = (() => {
       if (first && second && third) {
         if (first === second && second === third) {
           boxes.forEach((box) => {
-            box.removeEventListener("click", switcher);
+            box.removeEventListener("click", func);
           });
           winnerFound = true;
           toggle //declare winner depending on toggle position
@@ -61,29 +61,58 @@ const gameBoard = (() => {
   };
 
   // references the event.target, which is the box
-  function switcher(event) {
+  function playerMarker(event) {
     //if currentPlayer's mark is #, remove from array at box.id, replace with 'X'; update display; swap player;.
     switch (currentPlayer.mark) {
       case "X":
         gameBoard.gameboard.splice(event.target.id, 1, "X");
-        gameBoard.winner();
+        gameBoard.winner(playerMarker);
         displayController.displayUpdate();
         currentPlayer = playerSwapper();
-        event.target.removeEventListener("click", switcher);
+        event.target.removeEventListener("click", playerMarker);
         break;
       case "O":
         gameBoard.gameboard.splice(event.target.id, 1, "O");
-        gameBoard.winner();
+        gameBoard.winner(playerMarker);
         displayController.displayUpdate();
         currentPlayer = playerSwapper();
-        event.target.removeEventListener("click", switcher);
+        event.target.removeEventListener("click", playerMarker);
         break;
     }
   }
-  //sets up boxes for marking using ^ switcher
-  const marker = () => {
+
+  // references the event.target, which is the box
+  function aiMarker(event) {
+    //if currentPlayer is the user, mark the grid with user's mark.
+    switch (currentPlayer) {
+      case player1:
+        gameBoard.gameboard.splice(event.target.id, 1, player1.mark);
+        gameBoard.winner(aiMarker);
+        displayController.displayUpdate();
+        currentPlayer = playerSwapper();
+        event.target.removeEventListener("click", aiMarker);
+        break;
+      case player2: // if the currentPlayer is the computer, use minimax to mark?
+        // splice the return value from running minimax with the current board & computer as the player
+        let bestSpot = computer.minimax(gameBoard.gameboard, computer.aiPlayer);
+        console.log(bestSpot.index);
+        // gameBoard.gameboard.splice(
+        //   event.target.id,
+        //   1,
+        //   computer.minimax(gameBoard.gameboard, player2.mark)
+        // );
+        // gameBoard.winner(aiMarker);
+        // displayController.displayUpdate();
+        // currentPlayer = playerSwapper();
+        // event.target.removeEventListener("click", aiMarker);
+        break;
+    }
+  }
+
+  //enables marking on boxes using ^ switcher
+  const marker = (func) => {
     boxes.forEach((box) => {
-      box.addEventListener("click", switcher);
+      box.addEventListener("click", func);
     });
   };
 
@@ -91,6 +120,8 @@ const gameBoard = (() => {
     gameboard,
     marker,
     winner,
+    playerMarker,
+    aiMarker,
   };
 })();
 //end of IIFE
@@ -146,6 +177,9 @@ const gameInitializer = (() => {
   const userMarkX = document.getElementById("toggle-on");
   const replayButton = document.querySelector(".cta");
 
+  const computerButton = document.querySelector(".computer-button");
+
+  //reboot game
   const reboot = () => {
     let markers = document.querySelectorAll(".marker");
     winnerFound = false;
@@ -163,6 +197,21 @@ const gameInitializer = (() => {
     gameBoard.gameboard = Array(9).fill(undefined);
   };
 
+  //reset game
+  const reset = () => {
+    let markers = document.querySelectorAll(".marker");
+    currentPlayer = player1;
+    winnerFound = false;
+    toggle = false;
+    versus.innerText = "VS";
+    markers.forEach((marker) => {
+      marker.remove();
+    });
+    gameBoard.gameboard = Array(9).fill(undefined);
+    gameBoard.marker(gameBoard.playerMarker); // initializes gameboard for marking
+    displayController.displayUpdate();
+  };
+
   //create player
   user1Submit.addEventListener("click", function () {
     let name = userName.value;
@@ -174,7 +223,7 @@ const gameInitializer = (() => {
     displayUserName.innerText = player1.name + ", " + player1.mark;
   });
 
-  //create opponent, start game
+  //create opponent, start game against human player
   opponentSubmit.addEventListener("click", function () {
     let name = opponentName.value;
     player1.mark == "X"
@@ -184,24 +233,125 @@ const gameInitializer = (() => {
     currentPlayer = player1;
     opponentName.value = "";
     displayOpponentName.innerText = player2.name + ", " + player2.mark;
-    gameBoard.marker(); // initializes gameboard for marking
-    displayController.displayUpdate();
+    gameBoard.marker(gameBoard.playerMarker); // initializes gameboard for marking
+  });
+
+  computerButton.addEventListener("click", function () {
+    userMarkX.checked
+      ? (player2 = player("computer", "O"))
+      : (player2 = player("computer", "O"));
+
+    currentPlayer = player1;
+    computer.huPlayer = player1.mark;
+    computer.aiPlayer = player2.mark;
+
+    displayOpponentName.innerText = player2.name + ", " + player2.mark;
+    gameBoard.marker(gameBoard.aiMarker);
   });
 
   resetButton.addEventListener("click", reboot);
 
-  replayButton.addEventListener("click", function () {
-    let markers = document.querySelectorAll(".marker");
-    currentPlayer = player1;
-    winnerFound = false;
-    toggle = false;
-    versus.innerText = "VS";
-    markers.forEach((marker) => {
-      marker.remove();
-    });
-    gameBoard.gameboard = Array(9).fill(undefined);
-    gameBoard.marker(); // initializes gameboard for marking
-    displayController.displayUpdate();
-  });
+  replayButton.addEventListener("click", reset);
   return {};
+})();
+
+//AI PLAYER
+
+const computer = (() => {
+  //human
+  let huPlayer;
+  //ai
+  let aiPlayer;
+
+  // the main minimax function
+  function minimax(newBoard, player) {
+    //available spots
+    var availSpots = emptyIndexies(newBoard);
+
+    // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+    if (winning(newBoard, huPlayer)) {
+      return { score: -10 };
+    } else if (winning(newBoard, aiPlayer)) {
+      return { score: 10 };
+    } else if (availSpots.length === 0) {
+      return { score: 0 };
+    }
+
+    // an array to collect all the objects
+    var moves = [];
+
+    // loop through available spots
+    for (var i = 0; i < availSpots.length; i++) {
+      //create an object for each and store the index of that spot that was stored as a number in the object's index key
+      var move = {};
+      move.index = newBoard[availSpots[i]];
+
+      // set the empty spot to the current player
+      newBoard[availSpots[i]] = player;
+
+      //if collect the score resulted from calling minimax on the opponent of the current player
+      if (player == aiPlayer) {
+        var result = minimax(newBoard, huPlayer);
+        move.score = result.score;
+      } else {
+        var result = minimax(newBoard, aiPlayer);
+        move.score = result.score;
+      }
+
+      //reset the spot to empty
+      newBoard[availSpots[i]] = move.index;
+
+      // push the object to the array
+      moves.push(move);
+    }
+
+    // if it is the computer's turn loop over the moves and choose the move with the highest score
+    var bestMove;
+    if (player === aiPlayer) {
+      var bestScore = -10000;
+      for (var i = 0; i < moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      // else loop over the moves and choose the move with the lowest score
+      var bestScore = 10000;
+      for (var i = 0; i < moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+
+    // return the chosen move (object) from the array to the higher depth
+    return moves[bestMove];
+  }
+
+  // returns the available spots on the board
+  function emptyIndexies(board) {
+    return board.filter((s) => s == undefined);
+  }
+
+  // winning combinations using the board indexies for instace the first win could be 3 xes in a row
+  function winning(board, player) {
+    if (
+      (board[0] == player && board[1] == player && board[2] == player) ||
+      (board[3] == player && board[4] == player && board[5] == player) ||
+      (board[6] == player && board[7] == player && board[8] == player) ||
+      (board[0] == player && board[3] == player && board[6] == player) ||
+      (board[1] == player && board[4] == player && board[7] == player) ||
+      (board[2] == player && board[5] == player && board[8] == player) ||
+      (board[0] == player && board[4] == player && board[8] == player) ||
+      (board[2] == player && board[4] == player && board[6] == player)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return { minimax, huPlayer, aiPlayer, emptyIndexies, winning };
 })();
